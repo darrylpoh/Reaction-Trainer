936c453f706fe0e26418c3a8c5df92adf366073b
darrylpoh <darrylpoh.dp13@gmail.com>
2021-08-03 11:35:14 +0800

    Added timer for reaction time

--- src/components/Space.vue
+++ src/components/Space.vue
@@ -1,12 +1,38 @@
 <template>
-  <div class="space">
+  <div class="space" v-if="showSpace" @click="stopTimer">
       Click me!
   </div>
 </template>
 
 <script>
 export default {
-    props: ['delay']
+    props: ['delay'],
+    data() {
+        return {
+            showSpace: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+         this.showSpace = true,
+         this.startTimer()
+        }, this.delay)
+    },
+    methods: {
+        startTimer() {    
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)  
+
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            console.log(this.reactionTime)
+
+        }
+    }
 }
 </script>
 
