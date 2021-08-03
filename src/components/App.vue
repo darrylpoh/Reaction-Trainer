92130a82c8195db600984baa7c712a4a1fd3c6ce
darrylpoh <darrylpoh.dp13@gmail.com>
2021-08-03 19:22:25 +0800

    Latest update without results

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,38 +1,62 @@
 <template>
   <h1>Reaction Trainer</h1>
-  <button @click="start" :disabled="isTraining">begin</button>
-  <space v-if="isTraining" :delay="delay"/>
+  <button @click="start" :disabled="isTraining">play</button>
+  <box v-if="isTraining" :delay="delay" @end="endGame" />
+  <p v-if="showResults">Reaction time: {{ score }} ms</p>
 </template>
 
 <script>
-import Space from './Space.vue'
-
+import box from './box.vue'
 
 export default {
   name: 'App',
-  components: { Space },
+  components: { box },
   data() {
     return {
       isTraining: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
     start() {
-      this.delay = 1000 + Math.random() * 5000
+      // set time amount (ms)
+      this.delay = 2000 + Math.random() * 5000
       this.isTraining = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isTraining = false
+      this.showResults = true
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #444;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
+  }
+  button {
+    background: #f4f4;
+    color: black;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
 </style>
