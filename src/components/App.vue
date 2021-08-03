0c9e95f9e027d89115ea1b4771cc3c9a36d19eca
darrylpoh <darrylpoh.dp13@gmail.com>
2021-08-03 10:25:16 +0800

    Input new files

---
+++ src/components/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Reaction Trainer</h1>
+  <button @click="start">begin</button>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: { },
+  data() {
+    return {
+      isTraining: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 1000 + Math.random() * 5000
+      this.isTraining = true
+      console.log(this.delay)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
